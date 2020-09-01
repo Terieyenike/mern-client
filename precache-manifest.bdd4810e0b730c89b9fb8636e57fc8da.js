ac14ccbffd3037432106ef72be405a3668891ddf
Teri Eyenike <teyenike1@gmail.com>
2020-09-01 12:43:32 +0100

    Updates

---
+++ precache-manifest.bdd4810e0b730c89b9fb8636e57fc8da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b5ed6ca9363692da51f634e3a2d657f",
+    "url": "/mern-client/index.html"
+  },
+  {
+    "revision": "e22e727e380e3a85ff2d",
+    "url": "/mern-client/static/css/2.e85a0276.chunk.css"
+  },
+  {
+    "revision": "be2d808ba098cd3508f3",
+    "url": "/mern-client/static/css/main.e3bac293.chunk.css"
+  },
+  {
+    "revision": "e22e727e380e3a85ff2d",
+    "url": "/mern-client/static/js/2.efabb64b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mern-client/static/js/2.efabb64b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be2d808ba098cd3508f3",
+    "url": "/mern-client/static/js/main.dc94343f.chunk.js"
+  },
+  {
+    "revision": "bafb2c0746d13e05f2bc",
+    "url": "/mern-client/static/js/runtime-main.d1ed3a7a.js"
+  }
+]);
